bf09c3eed3122fd7c9917f5eed56ece4743e13eb
danielaguilera <draguilera@uc.cl>
2024-06-20 00:07:45 -0400

    added constants file

---
+++ Metadata.py
@@ -0,0 +1,14 @@
+PORT_ARDUINO = '/dev/ttyUSB0'
+BAUD_RATE_ARDUINO = 19200
+PORT_HC06 = '/dev/ttyS0'
+BAUD_RATE_HC06 = 115200
+TIMEOUT = 1.0
+
+BAUD_RATES = {1200: 1,
+              2400: 2,
+              4800: 3,
+              9600: 4,
+              19200: 5,
+              38400: 6,
+              57600: 7,
+              115200: 8}
